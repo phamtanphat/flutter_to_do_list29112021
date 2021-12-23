83c5bf10bf7a8e752746abdd1f498def13f1cfa6
Nguyen Ngoc Anh <nguyen.ngoc.anh@pecc5.com>
2021-12-23 16:16:23 +0700

    update item

--- lib/to_do_list_page.dart
+++ lib/to_do_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_to_do_list29112021/todo_model.dart';
 
 class ToDoListPage extends StatefulWidget {
   @override
@@ -8,10 +7,37 @@ class ToDoListPage extends StatefulWidget {
 }
 
 class _ToDoListPageState extends State<ToDoListPage> {
-  bool isShowForm = false;
-  void setShow(bool isShow) {
+  late bool isShowForm;
+  late List<ToDoModel> listToDoModels;
+
+  @override
+  void initState() {
+    isShowForm = false;
+    listToDoModels = [];
+    super.initState();
+  }
+
+  void setShowForm(bool isShowForm) {
     setState(() {
-      isShowForm = isShow;
+      this.isShowForm = isShowForm;
+    });
+  }
+
+  void addWork(ToDoModel toDoModel){
+    setState(() {
+      listToDoModels.add(toDoModel);
+    });
+  }
+
+  void deleteWork(int position){
+    setState(() {
+      listToDoModels.removeAt(position);
+    });
+  }
+
+  void updateWork(int position, ToDoModel toDoModel){
+    setState(() {
+      listToDoModels.replaceRange(position,position+1,[toDoModel]);
     });
   }
 
@@ -23,20 +49,125 @@ class _ToDoListPageState extends State<ToDoListPage> {
       ),
       body: Container(
         padding: EdgeInsets.all(8.0),
-        child: isShowForm ? showForm():showButton(),
+        child: Column(
+          children: [
+            shouldShowForm(isShowForm, setShowForm , addWork),
+            Expanded(
+              child: ListView.builder(
+                itemCount: listToDoModels.length,
+                itemBuilder: (context , position){
+                  return itemTodo(listToDoModels[position] , position ,deleteWork);
+                },
+              ),
+            )
+          ],
+        ),
         // child: showTest(),
       ),
     );
   }
 
+  Widget shouldShowForm(bool isShowForm, Function setShowForm, Function addWork) {
+    final TextEditingController textTitle = TextEditingController();
+    final TextEditingController textDescription = TextEditingController();
+    if (isShowForm) {
+      return Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.grey[200],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: textTitle,
+              decoration: InputDecoration(
+                  hintText: "Title",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: textDescription,
+              decoration: InputDecoration(
+                  hintText: "Description",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)))),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 50,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        String title = textTitle.text.toString();
+                        String description = textDescription.text.toString();
 
-  Widget showButton() {
+                        if(title.isEmpty || description.isEmpty){
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Ban chua truyen du thong tin")));
+                          return;
+                        }
+                        addWork(ToDoModel(title: title , description: description));
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.green),
+                        shape:
+                        MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              // side: BorderSide(color: Colors.red)
+                            )),
+                        fixedSize:
+                        MaterialStateProperty.all(Size.fromHeight(50)),
+                      ),
+                      child: Text(
+                        "Add work",
+                        style: TextStyle(fontSize: 25),
+                      )),
+                ),
+                Expanded(flex: 5, child: SizedBox()),
+                Expanded(
+                  flex: 45,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        setShowForm(false);
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                        shape:
+                        MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              // side: BorderSide(color: Colors.red)
+                            )),
+                        fixedSize:
+                        MaterialStateProperty.all(Size.fromHeight(50)),
+                      ),
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(fontSize: 25),
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
     return Container(
       width: double.infinity,
       height: 50,
       child: ElevatedButton(
           onPressed: () {
-            setShow(true);
+            setShowForm(true);
           },
           style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(Colors.green),
@@ -47,115 +178,114 @@ class _ToDoListPageState extends State<ToDoListPage> {
                   ))),
           child: Text(
             "+",
-            style: TextStyle(fontSize: 40,),
+            style: TextStyle(
+              fontSize: 40,
+            ),
           )),
     );
   }
 
-  Widget showForm() {
+  Widget itemTodo(ToDoModel toDoModel , int position , Function deleteWork) {
     return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: Colors.grey[200],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            decoration: InputDecoration(
-                hintText: "Title",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(5)))),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                hintText: "Description",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(5)))),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                flex: 50,
-                child: ElevatedButton(
-                    onPressed: () {
-
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                            // side: BorderSide(color: Colors.red)
-                          )),
-                      fixedSize: MaterialStateProperty.all(Size.fromHeight(50)),
+      child: InkWell(
+        onTap: (){
+          showDialog(
+              context: context,
+              builder: (context){
+                final TextEditingController textTitle = TextEditingController();
+                textTitle.text = toDoModel.title;
+                final TextEditingController textDescription = TextEditingController();
+                textDescription.text = toDoModel.description;
+                return Dialog(
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: textTitle,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(5)))),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          controller: textDescription,
+                          decoration: InputDecoration(
+                              hintText: "Description",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(5)))),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context, rootNavigator: true).pop();
+                                  // setShowForm(false);
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                                  shape:
+                                  MaterialStateProperty.all<RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                        // side: BorderSide(color: Colors.red)
+                                      )),
+                                  fixedSize:
+                                  MaterialStateProperty.all(Size.fromHeight(50)),
+                                ),
+                                child: Text(
+                                  "Cancel",
+                                  style: TextStyle(fontSize: 25),
+                                )),
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context, rootNavigator: true).pop();
+                                  if(textTitle.text.isEmpty || textDescription.text.isEmpty){
+                                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Ban chua truyen du thong tin")));
+                                      return;
+                                  }
+                                  updateWork(position, ToDoModel(title: textTitle.text, description: textDescription.text));
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                                  shape:
+                                  MaterialStateProperty.all<RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                        // side: BorderSide(color: Colors.red)
+                                      )),
+                                  fixedSize:
+                                  MaterialStateProperty.all(Size.fromHeight(50)),
+                                ),
+                                child: Text(
+                                  "Update",
+                                  style: TextStyle(fontSize: 25),
+                                ))
+                          ],
+                        )
+                      ],
                     ),
-                    child: Text(
-                      "Add work",
-                      style: TextStyle(fontSize: 25),
-                    )),
-              ),
-              Expanded(flex: 5, child: SizedBox()),
-              Expanded(
-                flex: 45,
-                child: ElevatedButton(
-                    onPressed: () {
-                      setShow(false);
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.red),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                            // side: BorderSide(color: Colors.red)
-                          )),
-                      fixedSize: MaterialStateProperty.all(Size.fromHeight(50)),
-                    ),
-                    child: Text(
-                      "Cancel",
-                      style: TextStyle(fontSize: 25),
-                    )),
-              ),
-            ],
+                  ),
+                );
+              }
+          );
+        },
+        child: ListTile(
+          title: Text(toDoModel.title),
+          subtitle: Text(toDoModel.description),
+          trailing: IconButton(
+            icon: Icon(Icons.delete , color: Colors.red),
+            onPressed: (){
+              deleteWork(position);
+            },
           ),
-          // SizedBox(
-          //   height: 5,
-          // ),
-        ],
+        ),
       ),
     );
   }
-
-  Widget showTest(){
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      color: Colors.grey[500],
-      padding: EdgeInsets.all(10),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("View"),
-          Flexible(
-            child: TextField(
-              decoration: InputDecoration(
-                  hintText: "Title",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)))),
-            ),
-          ),
-          SizedBox(height: 30,),
-        ],
-      ),
-    );
-  }
-
 }
-
---
+++ lib/todo_model.dart
@@ -0,0 +1,6 @@
+class ToDoModel{
+  late String title;
+  late String description;
+
+  ToDoModel({required this.title,required this.description});
+}
